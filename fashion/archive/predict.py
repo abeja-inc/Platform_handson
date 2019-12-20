69ec6221ce75df839a52c0fd572802bca8c26c3b
yushinna <yuminnakao@Yumin-Nakao.local>
2019-12-20 23:17:38 +0900

    Create predict.py

---
+++ fashion/archive/predict.py
@@ -0,0 +1,35 @@
+import os
+from keras.preprocessing import image
+from keras.models import load_model
+from keras.applications.imagenet_utils import preprocess_input
+import numpy as np
+from PIL import Image
+
+img_rows, img_cols = 224, 224
+model = load_model(os.path.join(os.environ.get('ABEJA_TRAINING_RESULT_DIR', '.'), 'model.h5'))
+
+
+def decode_predictions(result):
+    categories = {
+        0: 'compressor',
+        1: 'fan_motor',
+        2: 'filter',
+        3: 'louver_motor',
+        4: 'thermistor'
+    }
+    result_with_labels = [{"label": categories[i], "probability": score} for i, score in enumerate(result)]
+    return sorted(result_with_labels, key=lambda x: x['probability'], reverse=True)
+
+
+def handler(_iter, ctx):
+    for img in _iter:
+        img = Image.fromarray(img)
+        img = img.resize((img_rows, img_cols))
+
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x, mode='tf')
+
+        result = model.predict(x)[0]
+        sorted_result = decode_predictions(result.tolist())
+        yield {"result": sorted_result}
